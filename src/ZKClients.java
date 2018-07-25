0a2d859ca4c677b8acdb7f76bf07666f48ac0a5d
Administrator <Administrator@Liuzx>
2018-07-25 12:50:14 +0800

    ZookeepDemo

--- src/ZKClients.java
+++ src/ZKClients.java
@@ -1,21 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 public class ZKClients {
-	private static final int sessionTimeout = 30000;
+	private static final int sessionTimeout = 3000;
 	private static final String connectString = "192.168.40.20:2181,192.168.40.21:2181,192.168.40.22:2181";
 	ZooKeeper zk = null;
+	private static final String nodepath = "/servers";
+	public volatile List<String> ServicesList = new ArrayList<String>();
 
 	public void init() throws Exception {
 		zk = new ZooKeeper(connectString, sessionTimeout, new Watcher() {
 			@Override
 			public void process(WatchedEvent arg0) {
 				// TODO Auto-generated method stub
-				System.out.println(arg0.getType());
+				try {
+					zk.getChildren("/", true);
+				} catch (KeeperException | InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
 			}
 		});
+	}
+
+	private void getServiceList() throws KeeperException, InterruptedException {
+		// TODO Auto-generated method stub
+		List<String> childrens = zk.getChildren(nodepath, true);
+		for (String str : childrens) {
+			byte[] data = zk.getData(nodepath + "/" + str, true, null);
+			ServicesList.add(new String(data));
+		}
+		System.out.println(ServicesList);
+	}
+
+	public static void main(String[] args) throws Exception {
+		ZKClients zkClients = new ZKClients();
+		zkClients.init();
+		zkClients.getServiceList();
+
 	}
 
 }
--- src/ZKServices.java
+++ src/ZKServices.java
@@ -7,8 +7,8 @@ import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
 public class ZKServices {
-	private static final int sessionTimeout = 30000;
-	private static final String connectString = "192.168.40.58:2181,192.168.40.59:2181,192.168.40.60:2181";
+	private static final int sessionTimeout = 3000;
+	private static final String connectString = "192.168.40.20:2181,192.168.40.21:2181,192.168.40.22:2181";
 	ZooKeeper zk = null;
 	private static final String nodepath = "/servers";
 
@@ -33,29 +33,20 @@ public class ZKServices {
 	}
 
 	public void registerServer(String node, String hostname) throws KeeperException, InterruptedException {
-		zk.create(node, hostname.getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+		zk.create(node, hostname.getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
 	}
-	public void registerServer(String node) {
-		zk.cre
-	}
-	
 
 	public void handleBussiness(String hostname) {
-		System.out.println(hostname + "  is working......");
+		System.out.println(hostname + "  is working...");
+
 	}
 
 	public static void main(String[] args) throws Exception {
 		String nodelevel = nodepath + "/server";
-
 		ZKServices services = new ZKServices();
 		services.init();
-
-		if (services.exists(args[0])) {
-			System.out.println(args[0] + "  is already exist");
-		} else {
-			services.registerServer(args[0]);
-		}
-
+		services.registerServer(nodelevel, args[0]);
 		services.handleBussiness(args[0]);
+
 	}
 }
